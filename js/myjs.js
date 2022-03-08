a3ded663197e49cf19083fc21304ee276ef61d17
ZengHW <zx08xk@126.com>
2022-03-08 19:10:59 +0800

    增加轮播图

---
+++ js/myjs.js
@@ -0,0 +1,50 @@
+const change_img= document.querySelector(".prev-next").children;
+const backward = change_img[0];
+const forward = change_img[1];
+backward.style.zIndex = "20";
+forward.style.zIndex = "20";
+let list = document.querySelectorAll("ul.img-list li");
+
+let current_id = 0;
+list[current_id].style.zIndex = "0";
+
+backward.onclick = function () {
+    list[current_id].style.zIndex = "0";
+    if (current_id === 0) {
+        current_id = 3;
+    }
+    else
+        current_id -= 1;
+    list[current_id].style.zIndex = "2";
+}
+
+forward.onclick = function () {
+    list[current_id].style.zIndex = "0";
+    if (current_id === 3) {
+        current_id = 0;
+    }
+    else
+        current_id += 1;
+    list[current_id].style.zIndex = "2";
+}
+
+const pointer_list = document.querySelectorAll(".banner .pointer a");
+let current_pointer = 2;
+pointer_list[current_pointer].style.backgroundColor = "rgb(255,255,255,0.4)";
+pointer_list[current_pointer].style.border = "2px rgb(0,0,0,0.4) solid";
+
+for (let i = 0; i < pointer_list.length; i++) {
+    pointer_list[i].onclick = function () {
+        pointer_list[current_pointer].style.backgroundColor = "rgb(0,0,0,0.4)";
+        pointer_list[current_pointer].style.border = "2px rgb(255,255,255,0.4) solid";
+        current_pointer = i;
+
+        list[current_id].style.zIndex = "0";
+        current_id = i;
+        list[current_id].style.zIndex = "2";
+
+        pointer_list[current_pointer].style.backgroundColor = "rgb(255,255,255,0.4)";
+        pointer_list[current_pointer].style.border = "2px rgb(0,0,0,0.4) solid";
+
+    }
+}
