15c9c1d5643b3ec47b4a7a6b545aaec3bb4f6a63
ZengHW <zx08xk@126.com>
2022-03-09 14:01:23 +0800

    轮播图

--- js/myjs.js
+++ js/myjs.js
@@ -2,47 +2,58 @@ const change_img= document.querySelector(".prev-next").children;
 const backward = change_img[0];
 const forward = change_img[1];
 let list = document.querySelectorAll("ul.img-list li");
+const pointer_list = document.querySelectorAll(".banner .pointer a");
+
+
 
 let current_id = 0;
-list[current_id].style.zIndex = "0";
+
+pointer_list[current_id].style.backgroundColor = "rgb(255,255,255,0.4)";
+pointer_list[current_id].style.border = "2px rgb(0,0,0,0.4) solid";
+list[current_id].style.zIndex = "2";
 
 backward.onclick = function () {
     list[current_id].style.zIndex = "0";
+    pointer_list[current_id].style.backgroundColor = "rgb(0,0,0,0.4)";
+    pointer_list[current_id].style.border = "2px rgb(255,255,255,0.4) solid";
     if (current_id === 0) {
         current_id = 3;
     }
     else
         current_id -= 1;
+    pointer_list[current_id].style.backgroundColor = "rgb(255,255,255,0.4)";
+    pointer_list[current_id].style.border = "2px rgb(0,0,0,0.4) solid";
     list[current_id].style.zIndex = "2";
 }
 
 forward.onclick = function () {
     list[current_id].style.zIndex = "0";
+    pointer_list[current_id].style.backgroundColor = "rgb(0,0,0,0.4)";
+    pointer_list[current_id].style.border = "2px rgb(255,255,255,0.4) solid";
     if (current_id === 3) {
         current_id = 0;
     }
     else
         current_id += 1;
+    pointer_list[current_id].style.backgroundColor = "rgb(255,255,255,0.4)";
+    pointer_list[current_id].style.border = "2px rgb(0,0,0,0.4) solid";
     list[current_id].style.zIndex = "2";
 }
 
-const pointer_list = document.querySelectorAll(".banner .pointer a");
-let current_pointer = 2;
-pointer_list[current_pointer].style.backgroundColor = "rgb(255,255,255,0.4)";
-pointer_list[current_pointer].style.border = "2px rgb(0,0,0,0.4) solid";
+
 
 for (let i = 0; i < pointer_list.length; i++) {
     pointer_list[i].onclick = function () {
-        pointer_list[current_pointer].style.backgroundColor = "rgb(0,0,0,0.4)";
-        pointer_list[current_pointer].style.border = "2px rgb(255,255,255,0.4) solid";
-        current_pointer = i;
+        pointer_list[current_id].style.backgroundColor = "rgb(0,0,0,0.4)";
+        pointer_list[current_id].style.border = "2px rgb(255,255,255,0.4) solid";
+
 
         list[current_id].style.zIndex = "0";
         current_id = i;
         list[current_id].style.zIndex = "2";
 
-        pointer_list[current_pointer].style.backgroundColor = "rgb(255,255,255,0.4)";
-        pointer_list[current_pointer].style.border = "2px rgb(0,0,0,0.4) solid";
+        pointer_list[current_id].style.backgroundColor = "rgb(255,255,255,0.4)";
+        pointer_list[current_id].style.border = "2px rgb(0,0,0,0.4) solid";
 
     }
 }
